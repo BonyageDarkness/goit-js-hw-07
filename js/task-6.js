21ef19f6501f65dcbbcc3cf0acff5acaa3f11f19
Bohdan <bosrok.ke@gmail.com>
2024-05-04 11:53:36 -0230

    6

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,78 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector('[data-create');
+const destroyBtn = document.querySelector('[data-destroy]');
+const input = document.querySelector('input');
+const box = document.querySelector('#boxes');
+const container = document.querySelector('#controls');
+
+const createBoxes = (amount) => {
+    let size = 30;
+    box.innerHTML = '';
+    input.value = '';
+    for (let i = 0; i < amount; i++) {
+        const boxen = document.createElement('div');
+        boxen.style.height = `${size}px`;
+        boxen.style.width = `${size}px`;
+        boxen.style.backgroundColor = getRandomHexColor();
+        box.appendChild(boxen);
+        size += 10;
+    }
+};
+
+createBtn.addEventListener('click', () => {
+    const item = parseInt(input.value);
+
+    if (item >= 1 && item <= 100) {
+        createBoxes(item);
+        input.value = '';
+    }
+});
+
+destroyBtn.addEventListener('click', () => {
+    destroyBoxes();
+});
+
+const destroyBoxes = () => {
+    box.innerHTML = '';
+};
+
+container.style.margin = '32px';
+container.style.display = 'flex';
+container.style.gap = '16px';
+
+input.style.display = 'flex';
+input.style.padding = '8px 72px';
+input.style.flexDirection = 'column';
+input.style.justifyContent = 'center';
+input.style.alignItems = 'center';
+input.style.gap = '10px';
+input.style.borderRadius = '8px';
+input.style.border = '1px solid #808080';
+
+destroyBtn.style.display = 'flex';
+destroyBtn.style.width = '120px';
+destroyBtn.style.padding = '8px 16px';
+destroyBtn.style.justifyContent = 'center';
+destroyBtn.style.alignItems = 'center';
+destroyBtn.style.gap = '10px';
+destroyBtn.style.borderRadius = '8px';
+destroyBtn.style.background = '#FF4E4E';
+
+createBtn.style.display = 'flex';
+createBtn.style.width = '120px';
+createBtn.style.padding = '8px 16px';
+createBtn.style.justifyContent = 'center';
+createBtn.style.alignItems = 'center';
+createBtn.style.gap = '10px';
+createBtn.style.borderRadius = '8px';
+createBtn.style.background = '#4E75FF';
+
+box.style.display = 'flex';
+box.style.gap = '44px';
+box.style.flexDirection = 'row';
+box.style.flexWrap = 'wrap';
